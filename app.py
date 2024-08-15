2d11b87d598ee5100efd1b97c92aa08c544ea56c
unknown <santoshbihar1502@gmail.com>
2024-08-15 12:44:23 +0530

    added two new fiture

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title(' This project for GIT Learning ')
+st.sidebar.title('This is SIDE BAR Menu')
+col1 , col2 = st.columns(2)
+
+with col1:
+    st.write(' This is columns first')
+with col2:
+    st.write(' This is columns Two')
+
+st.header('Course offered')
+st.subheader('BCA')
